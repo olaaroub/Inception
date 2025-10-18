ec2ddc0696eb844aadb92dd65496ad5e37d35449
olaaroub <oussamalaarroubi@gmail.com>
2025-10-18 15:57:42 -0400

    fixed some errors in mdb and finished portainer and adminer

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -7,6 +7,7 @@ service mariadb start;
 while ! mysqladmin ping -h localhost --silent; do
     sleep 1
 done
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
@@ -14,4 +15,4 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'
 
 mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
-exec "$@"  
+exec "$@"
